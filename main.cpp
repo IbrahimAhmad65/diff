0e2da37961102db5779c5a12101dfa904de07a66
Ibrahim Ahmad <ibrahim.algalaly@gmail.com>
2024-12-30 08:13:54 -0500

    lots o progress, collapse negative signes under multiplication now

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "Negation.h"
 #include <Addition.h>
 #include <Multiplication.h>
 #include <Number.h>
@@ -7,28 +8,10 @@
 #include <iostream>
 #include <memory>
 int main() {
-  Number num1 = Number(5);
-  Number num2 = Number(6);
   Variable x = Variable('x');
-  std::cout << "making vector" << std::endl;
-  auto k = std::vector<std::shared_ptr<Group>>{std::make_shared<Number>(num1),
-                                               std::make_shared<Number>(num2),
-                                               std::make_shared<Variable>(x)};
-  std::cout << "making addition" << std::endl;
-  Addition add = Addition(k);
-  k.push_back(std::make_shared<Addition>(add));
-  Multiplication mult = Multiplication(k);
-  auto multiplied = mult.applyDeep();
-  std::cout << mult << std::endl;
-  std::cout << *multiplied << std::endl;
-  std::cout << num2 << std::endl;
-  std::ofstream file("latex_output/output.tex");
-  if (file.is_open()) {
-    multiplied->build_latex(file) << std::endl;
-    file.close();
-    std::cout << "LaTeX file 'output.tex' has been created." << std::endl;
-  } else {
-    std::cerr << "Failed to open the file for writing." << std::endl;
-  }
+  auto add = (-x * -6) + 2;
+  std::cout << *add << std::endl;
+  auto applied = add->applyDeep();
+  std::cout << *applied << std::endl;
   return 0;
 }
